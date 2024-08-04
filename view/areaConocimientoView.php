01a6bceac22a59f196477c6b2f0e0cdaa41f0571
Kevin <kvenegasbermudez@gmail.com>
2024-08-04 16:49:17 -0600

    Cambios en la view area conocimiento

--- view/areaConocimientoView.php
+++ view/areaConocimientoView.php
@@ -6,14 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>LUV</title>
   <script>
-    function deleteConfirmation(){
-      var response = confirm("¿Desea eliminar esta área de conocimiento?");
+    function actionConfirmation(mensaje){
+      var response = confirm(mensaje);
       return response;
     }
 
-    function updateConfirmation(){
-      var response = confirm("¿Desea actualizar el nombre de esta área de conocimiento?");
-      return response;
+    function showMessage(mensaje){
+      alert(mensaje);
     }
   </script>
 </head>
@@ -26,6 +25,32 @@
   </header>
 
   <div class="container mt-3">
+    <section id="alerts">
+      <?php
+        if (isset($_GET['error'])) {
+          $mensaje = "Ocurrió un error debido a ";
+          $mensaje .= match(true){
+            $_GET['error']=="emptyField" => "campo(s) vacío(s).",
+            $_GET['error']=="numberFormat" => "ingreso de valores númericos.",
+            $_GET['error']=="dbError" => "un problema al procesar la transacción.",
+            $_GET['error']=="exist" => "que dicho nombre ya existe.",
+            default => "un problema inesperado.",
+          };
+        } else if (isset($_GET['success'])) {
+            $mensaje = match(true){
+              $_GET['success']=="inserted" => "Área de conocimiento creada correctamente.",
+              $_GET['success']=="updated" => "Área de conocimiento actualizada correctamente.",
+              $_GET['success']=="deleted" => "Área de conocimiento eliminada correctamente.",
+              default => "Transacción realizada.",
+            };
+        }
+
+        if(isset($mensaje)){
+          echo "<script>showMessage('$mensaje')</script>";
+        }
+      ?>
+    </section>
+
     <section id="form">
       <div class="container">
 
@@ -48,7 +73,7 @@
             <div class="row mb-3">
               <div class="col">
                 <label for="descripcion" class="form-label">Descripción: </label>
-                <textarea required type="text" name="descripcion" id="descripcion" class="form-control" placeholder="Descripción del área de conocimiento" rows="3"></textarea>
+                <textarea required name="descripcion" id="descripcion" class="form-control" placeholder="Descripción del área de conocimiento" rows="3"></textarea>
               </div>
             </div>
 
@@ -80,6 +105,8 @@
           include '../bussiness/areaConocimientoBussiness.php';
           $areaConocimientoBussiness = new AreaConocimientoBussiness();
           $areasConocimiento = $areaConocimientoBussiness->getAllTbAreaConocimiento();
+          $mensajeActualizar = "¿Desea actualizar esta área de conocimiento?";
+          $mensajeEliminar = "¿Desea eliminar esta área de conocimiento?";
 
           if ($areasConocimiento != null) {
             foreach ($areasConocimiento as $areaConocimiento) {
@@ -90,8 +117,8 @@
               echo '<td><input type="text" name="nombre" id="nombre" value="' . htmlspecialchars($areaConocimiento->getTbAreaConocimientoNombre()) . '" class="form-control" /></td>';
               echo '<td><input type="text" name="descripcion" id="descripcion" value="' . htmlspecialchars($areaConocimiento->getTbAreaConocimientoDescripcion()) . '" class="form-control" /></td>';
               echo '<td>';
-              echo '<button type="submit" class="btn btn-warning me-2" name="update" id="update" onclick="return updateConfirmation()">Actualizar</button>';
-              echo '<button type="submit" class="btn btn-danger" name="delete" id="delete" onclick="return deleteConfirmation()">Eliminar</button>';
+              echo "<button type='submit' class='btn btn-warning me-2' name='update' id='update' onclick='return actionConfirmation(\"$mensajeActualizar\")'>Actualizar</button>";
+              echo "<button type='submit' class='btn btn-danger' name='delete' id='delete' onclick='return actionConfirmation(\"$mensajeEliminar\")'>Eliminar</button>";
               echo '</td>';
               echo '</form>';
               echo '</tr>';
