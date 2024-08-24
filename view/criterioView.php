bc5c8576ef8d59d733de5b1cce3632fbbed65351
Jamyr González <josuegonza1702@gmail.com>
2024-08-23 19:13:37 -0600

    Funciones de sesión en Criterio y Valor

--- view/criterioView.php
+++ view/criterioView.php
@@ -1,9 +1,5 @@
 <?php
-  session_start();
-      
-  if ($_SESSION["tipoUsuario"] == "Usuario" || empty($_SESSION["tipoUsuario"])) {
-      header("location: ./login.php?error=accessDenied");
-  }
+  include "../action/sessionAction.php";
 ?>
 
 <!DOCTYPE html>
@@ -68,7 +64,11 @@
     <section id="form">
       <div class="container">
         
-      <button onclick="window.location.href='../indexView.php';">Volver</button>
+        <button onclick="window.location.href='../indexView.php';">Volver</button>
+        <form method="post" action="../action/sessionAction.php">
+          <button type="submit" class="btn btn-success" name="logout" id="logout">Cerrar sesión</button>
+        </form>
+
         <div class="text-center mb-4">
             <h3>Agregar un nuevo criterio</h3>
             <p class="text-muted">Complete el formulario para añadir un nuevo criterio</p>
