c09d134f683d1d40f4a9dedb09b4c4e4ca70f339
Jamyr González <josuegonza1702@gmail.com>
2024-08-26 14:10:07 -0600

    Mejora Index

--- indexView.php
+++ indexView.php
@@ -1,5 +1,5 @@
 <?php
-    include_once 'action/sessionAction.php';
+    include_once 'action/sessionAdminAction.php';
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +16,7 @@
     <div id="cruds">
         <h3>Index</h3>  
         <table id="tabla">
-            <th>CRUD</th>
+            <th>Instituciones</th>
             <tr>
                 <td>
                     <a href="view/universidadView.php">Universidad</a>
@@ -27,21 +27,7 @@
                     <a href="view/campusView.php">Campus</a>
                 </td> 
             </tr>
-            <tr>
-                <td>
-                    <a href="view/areaConocimientoView.php">Área de Conocimiento</a>
-                </td> 
-            </tr>
-            <tr>
-                <td>
-                    <a href="view/orientacionSexualView.php">Orientación Sexual</a>
-                </td> 
-            </tr>
-            <tr>
-                <td>
-                    <a href="view/generoView.php">Género</a>
-                </td> 
-            </tr>
+            <th>Aspectos de Campus</th>
             <tr>
                 <td>
                     <a href="view/campusColectivoView.php">Campus Colectivo</a>
@@ -57,6 +43,23 @@
                     <a href="view/campusEspecializacionView.php">Campus Especialización</a>
                 </td> 
             </tr>
+            <tr>
+                <td>
+                    <a href="view/areaConocimientoView.php">Área de Conocimiento</a>
+                </td> 
+            </tr>
+            <th>Personalidad</th>
+            <tr>
+                <td>
+                    <a href="view/orientacionSexualView.php">Orientación Sexual</a>
+                </td> 
+            </tr>
+            <tr>
+                <td>
+                    <a href="view/generoView.php">Género</a>
+                </td> 
+            </tr>
+            <th>Administrativo</th>
             <tr>
                 <td>
                     <a href="view/imagenView.php">Imagen</a>
