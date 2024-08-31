41699f9a5570f87e1235bf9fe46938440f716773
Jamyr González <josuegonza1702@gmail.com>
2024-08-31 15:45:57 -0600

    Implementado autocompletado en perfil deseado

--- view/userWantedProfileView.php
+++ view/userWantedProfileView.php
@@ -9,6 +9,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LUV Perfil Deseado</title>
+
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 </head>
 <body data-view="WantedProfile">
     <section id="alerts">
